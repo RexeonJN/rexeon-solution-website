a5b70edb8fd8c47f7719122ccbe11160aec8764e
alka <alkar1846@mail.com>
2021-09-11 13:33:44 +0530

    complete home page

---
+++ rexeonapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RexeonappConfig(AppConfig):
+    name = 'rexeonapp'
---
+++ rexeonapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'rexeonapp/home.html')
+
+def about(request):
+    return render(request,'rexeonapp/about.html')
